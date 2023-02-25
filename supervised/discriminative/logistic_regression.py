45e6eea42bd76f0dcf210c067f93c321ef1140fa
ChenQirui1 <ivanchen.code@gmail.com>
2023-02-25 17:49:13 +0800

    Initial commit

---
+++ supervised/discriminative/logistic_regression.py
@@ -0,0 +1,113 @@
+import numpy as np
+import utils.util as util
+
+from linear_model import LinearModel
+
+
+def main(train_path, eval_path, pred_path):
+    """Problem 1(b): Logistic regression with Newton's Method.
+
+    Args:
+        train_path: Path to CSV file containing dataset for training.
+        eval_path: Path to CSV file containing dataset for evaluation.
+        pred_path: Path to save predictions.
+    """
+    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+    x_eval,y_eval = util.load_dataset(eval_path, add_intercept=True)
+
+    # *** START CODE HERE ***
+    clf = LogisticRegression()
+    clf.fit(x_train,y_train)
+    clf.score(x_eval,y_eval)
+    #prediction = clf.predict(x_eval)
+    # with open(pred_path,"w+") as f:
+    #     for line in prediction:
+    #         f.write(str(line)+"\n")
+
+    # *** END CODE HERE ***
+
+class LogisticRegression(LinearModel):
+    """Logistic regression with Newton's Method as the solver.
+
+    Example usage:
+        > clf = LogisticRegression()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+
+    def fit(self, x, y):
+        """Run Newton's Method to minimize J(theta) for logistic regression.
+
+        Args:
+            x: Training example inputs. Shape (m, n).
+            y: Training example labels. Shape (m,).
+        """
+        # *** START CODE HERE ***
+        #declaring X and y
+        X = np.array(x,dtype=np.float64)
+        Y = np.array(y,dtype=np.float64)
+        
+        #declare the observation size and no's of features
+        m, n = np.shape(X)
+        self.theta = np.zeros(n)
+
+        #vectorised function of g(x) based on design matrix X
+        def g(X):
+            array = np.empty(m,dtype=np.float64)
+            
+            for i in range(m):
+                array[i] = 1/(1+np.exp(-(np.dot(self.theta,X[i]))))
+
+            return array
+
+        #loop for convergence
+        prev_theta = np.ones(n)
+        while np.linalg.norm(prev_theta - self.theta) > self.eps:
+            #! -= operator does not work with vectorised form
+            #hessian of likelihood func
+            hessian = (1/m)*np.dot((X.T*(g(X)*(1-g(X)))),X)
+            self.theta = self.theta - np.dot(np.linalg.inv(hessian),(1/m)*np.dot(X.T,(g(X)-Y)))
+            prev_theta = self.theta
+
+        return self.theta
+        # *** END CODE HERE ***
+
+    def predict(self, x):
+        """Make a prediction given new inputs x.
+
+        Args:
+            x: Inputs of shape (m, n).
+
+        Returns:
+            Outputs of shape (m,).
+        """
+        # *** START CODE HERE ***
+        X = np.array(x)
+        m,n = np.shape(X)
+        output = np.empty(m,dtype=np.float64)
+        raw_value = 0
+        for i in range(m):
+            raw_value = 1/(1+np.exp(-(np.dot(self.theta,X[i]))))
+            output[i] = 1 if (raw_value > 0.5) else 0
+        return output
+
+        # *** END CODE HERE ***
+
+    def score(self,x,y):
+        prediction = self.predict(x)
+        confusion_matrix = np.empty((2,2))
+        for i in range(len(prediction)):
+            if prediction[i] == y[i] and y[i] == 1:
+                confusion_matrix[0][0] += 1
+            elif prediction[i] != y[i] and y[i] == 1:
+                confusion_matrix[0][1] += 1
+            elif prediction[i] != y[i] and y[i] == 0:
+                confusion_matrix[1][0] += 1
+            elif prediction[i] == y[i] and y[i] == 0:
+                confusion_matrix[1][1] += 1
+
+        #scoring by accuracy
+        score = (confusion_matrix[0][0] + confusion_matrix[1][1]) / np.sum(confusion_matrix)
+        print(confusion_matrix)
+        print(score)
+        return score
---
+++ supervised/generative/gda.py
@@ -0,0 +1,118 @@
+import numpy as np
+import utils.util as util
+from scipy.stats import multivariate_normal 
+from supervised.discriminative.linear_model import LinearModel
+
+
+def main(train_path, eval_path, pred_path):
+    """Problem 1(e): Gaussian discriminant analysis (GDA)
+
+    Args:
+        train_path: Path to CSV file containing dataset for training.
+        eval_path: Path to CSV file containing dataset for evaluation.
+        pred_path: Path to save predictions.
+    """
+    # Load dataset
+    x_train, y_train = util.load_dataset(train_path, add_intercept=False)
+    x_eval,y_eval = util.load_dataset(eval_path, add_intercept=False)
+
+    # *** START CODE HERE ***
+    clf = GDA()
+    clf.fit(x_train,y_train)
+    print(clf.predict(x_eval))
+    # *** END CODE HERE ***
+
+class GDA(LinearModel):
+    """Gaussian Discriminant Analysis.
+
+    Example usage:
+        > clf = GDA()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+
+    def fit(self, x, y):
+        """Fit a GDA model to training set given by x and y
+
+        Args:
+            x: Training example inputs. Shape (m, n).
+            y: Training example labels. Shape (m,).
+
+        Returns:
+            theta: GDA model parameters.
+        """
+
+        
+        # *** START CODE HERE ***
+
+        def covariance_matrix(x):
+            x_true = x[np.nonzero(y)]
+            x_true = x_true - mu1
+            x_true = np.repeat(x_true,n,axis=1) * np.tile(x_true,n)
+
+
+            x_false = x[np.where(y==0)]
+            x_false = x_false - mu0
+            x_false = np.repeat(x_false,n,axis=1) * np.tile(x_false,n)
+
+
+            x_combine = np.vstack((x_true,x_false))
+            total = np.sum(x_combine, axis=0)/m
+            matrix = np.reshape(total,(n,n))
+
+            return matrix
+
+        
+        m,n = np.shape(x)
+
+        phi = sum(y)/m
+        mu1 = np.sum(x[np.nonzero(y)],axis=0)/sum(y)
+        mu0 = np.sum(x[np.where(y==0)],axis=0)/(m-sum(y))
+        sigma = covariance_matrix(x)
+
+        theta = {
+            "phi": phi,
+            "mu0": mu0,
+            "mu1" : mu1,
+            "sigma" : sigma,
+        }
+
+        self.theta = theta
+
+        return self.theta
+
+        # *** END CODE HERE ***
+
+    def predict(self, x):
+        """Make a prediction given new inputs x.
+
+        Args:
+            x: Inputs of shape (m, n).
+
+        Returns:
+            Outputs of shape (m,).
+        """
+        # *** START CODE HERE ***
+        phi = self.theta['phi']
+        mu0 = self.theta['mu0']
+        mu1 = self.theta['mu1']
+        sigma = self.theta['sigma']
+
+        prob_x_given_y_true = multivariate_normal.pdf(x[1,:],mean=mu1,cov=sigma)
+        prob_x_given_y_false = multivariate_normal.pdf(x[1,:],mean=mu0,cov=sigma)
+
+        #for prediction in probability, use the following
+        #posterior = (prob_x_given_y_true*phi)/((prob_x_given_y_true*phi)+(prob_x_given_y_false*phi))
+
+        prediction = []
+
+        for feat_vec in x:
+            prob_x_given_y_false = multivariate_normal.pdf(feat_vec,mean=mu0,cov=sigma)
+            prob_x_given_y_true = multivariate_normal.pdf(feat_vec,mean=mu1,cov=sigma)
+
+            prediction.append(np.argmax([prob_x_given_y_false,prob_x_given_y_true]))
+
+
+        return prediction
+
+
